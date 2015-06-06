56de0c84b577680ea300255f4d14a74b8114e819
Regina <laulessr@gmail.com>
2015-06-06 17:09:59 -0500

    Changed nurses, started clinics

---
+++ nurses/destroy.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reginaimhoff
+ * Date: 6/5/15
+ * Time: 6:11 PM
+ */
+
+error_reporting(E_ALL);
+
+session_start();
+
+if(!isset($_SESSION['id'])){
+    //redirect them back to login page
+    header("Location: ../session/new.php"); /* Redirect browser */
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+}
+
+$nurse_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+if ($nurse_id === null) {
+    http_response_code(422);
+    header('Content-type: application/json');
+    $response_array = array(
+        'status' => 'error',
+        'message' => 'Nurse ID not given!'
+    );
+    echo json_encode($response_array);
+    exit;
+}
+
+include '../configuration.php';
+
+// Create connection
+$connection = new mysqli(
+    $database_configuration['servername'],
+    $database_configuration['username'],
+    $database_configuration['password'],
+    $database_configuration['database']
+);
+
+if ($connection->connect_error) {
+    http_response_code(500);
+    header('Content-type: application/json');
+    $response_array = array(
+        'status' => 'error',
+        'message' => 'Try again later (1)'
+    );
+    echo json_encode($response_array);
+    exit;
+}
+
+if (!($statement = $connection->prepare("DELETE FROM nurses WHERE nurses.id = ?"))) {
+    error_log($connection->error);
+    http_response_code(500);
+    header('Content-type: application/json');
+    $response_array = array(
+        'status' => 'error',
+        'message' => 'Try again later (2)'
+    );
+    echo json_encode($response_array);
+    exit;
+}
+
+if (!$statement->bind_param('i', $nurse_id)) {
+    header('Content-type: application/json');
+    $response_array = array(
+        'status' => 'error',
+        'message' => 'Try again later (3)'
+    );
+    echo json_encode($response_array);
+    exit;
+}
+
+if (!$statement->execute()) {
+    error_log($statement->error);
+
+    http_response_code(500);
+    header('Content-type: application/json');
+    $response_array = array(
+        'status' => 'error',
+        'message' => 'Try again later (4)'
+    );
+    echo json_encode($response_array);
+    exit;
+}
+$statement->close();
+header('Content-type: application/json');
+$response_array = array(
+    'status' => 'success',
+    'id' => $nurse_id,
+    'message' => 'Nurse removed'
+);
+echo json_encode($response_array);
+exit;
+?>
---
+++ nurses/edit.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reginaimhoff
+ * Date: 6/5/15
+ * Time: 6:11 PM
+ */
+
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    //redirect them back to login page
+    header("Location: ../session/new.php"); /* Redirect browser */
+    exit();
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Edit Yarn</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/nurses/new.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://malsup.github.com/jquery.form.js"></script>
+
+    <script>
+        // wait for the DOM to be loaded
+        $(document).ready(function () {
+            var options = {
+                error: function (xhr, statusText, errorThrown) {
+                    $('#form-errors').html(xhr.responseJSON.message);
+                },
+                success: function (responseJSON, statusText, xhr, formElement) {
+                    $(location).attr('href', 'index.php');
+                }
+            };
+            $('#form').ajaxForm(options);
+        });
+    </script>
+</head>
+
+<body>
+<?php
+$nurse_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+if ($nurse_id === null) {
+    ?>
+    <p>No nurse id given to edit!</p>
+    <?php
+    exit;
+}
+
+include '../configuration.php';
+
+// Create connection
+$connection = new mysqli(
+    $database_configuration['servername'],
+    $database_configuration['username'],
+    $database_configuration['password'],
+    $database_configuration['database']
+);
+
+if (!($statement = $connection->prepare(
+    "SELECT nurses.id, " .
+    "nurses.first_name, " .
+    "nurses.last_name, " .
+    "FROM nurses " .
+    "WHERE nurses.id = ?"
+))
+) {
+    error_log($connection->error);
+    ?>
+    <p>Try again later (1)</p>
+    <?php
+    exit;
+}
+
+if (!$statement->bind_param('i', $nurse_id)) {
+    error_log($statement->error);
+    ?>
+    <p>Try again later (2)</p>
+    <?php
+    exit;
+}
+
+
+if (!$statement->execute()) {
+    error_log($statement->error);
+    ?>
+    <p>Try again later (3)</p>
+    <?php
+    exit;
+}
+
+$out_id = null;
+$out_first_name = null;
+$out_last_name = null;
+$out_dob = null;
+
+if (!$statement->bind_result($out_id, $out_first_name, $out_last_name, $out_dob)) {
+    error_log($statement->error);
+    ?>
+    <p>Try again later (4)</p>
+    <?php
+    exit;
+}
+while ($statement->fetch()) {
+    ?>
+    <div class="container">
+
+        <form action="update.php" class="form-signin" id="form" method="post">
+            <h2 class="form-signin-heading">Edit Nurse</h2>
+            <input type="hidden" name="id" value="<?php echo $out_id ?>">
+
+            <label for="first_name" class="sr-only">First Name</label>
+            <input type="text" id="first_name" class="form-control" placeholder="First Name" required autofocus
+                   name="first_name" value="<?php echo $out_first_name ?>">
+
+            <label for="last_name" class="sr-only">Last Name</label>
+            <input type="text" id="last_name" class="form-control" placeholder="Last Name" required autofocus
+                   name="last_name" value="<?php echo $out_last_name ?>">
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Update Nurse</button>
+
+            <p id="form-errors">
+
+            </p>
+        </form>
+    </div> <!-- /container -->
+
+<?php
+}
+?>
+</body>
+</html>
