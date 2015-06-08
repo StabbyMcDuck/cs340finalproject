350831f483e723f5d6546390d532f93f4ef91e8e
Regina <laulessr@gmail.com>
2015-06-07 20:43:49 -0500

    Use navbar.php throughout blood bag pages

--- blood_bags/edit.php
+++ blood_bags/edit.php
@@ -55,34 +55,9 @@ if (!isset($_SESSION['id'])) {
 </head>
 
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="../index.php">Blood Bank</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-left">
-                <li><a href="../blood_bags/index.php">Blood Bags</a></li>
-                <li><a href="../blood_types/index.php">Blood Types</a></li>
-                <li><a href="../clinics/index.php">Clinics</a></li>
-                <li><a href="../donors/index.php">Donors</a></li>
-                <li><a href="../nurses/index.php">Nurses</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="../session/destroy.php">Logout</a></li>
-            </ul>
-        </div>
-        <!--/.nav-collapse -->
-    </div>
-</nav>
 <?php
+require '../navbar.php';
+
 $blood_bag_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 if ($blood_bag_id === null) {
     ?>
