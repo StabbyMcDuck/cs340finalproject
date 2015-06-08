e5e7b538be8145da6a826b4d52d385efc01406e8
Regina <laulessr@gmail.com>
2015-06-07 20:24:40 -0500

    Add all indexes to navbar

--- blood_bags/index.php
+++ blood_bags/index.php
@@ -68,9 +68,16 @@ if (!isset($_SESSION['id'])) {
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Blood Bags List</a>
+            <a class="navbar-brand" href="../index.php">Blood Bank</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="../blood_bags/index.php">Blood Bags</a></li>
+                <li><a href="../blood_types/index.php">Blood Types</a></li>
+                <li><a href="../clinics/index.php">Clinics</a></li>
+                <li><a href="../donors/index.php">Donors</a></li>
+                <li><a href="../nurses/index.php">Nurses</a></li>
+            </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="new.php">Add Blood Bag</a></li>
                 <li><a href="../session/destroy.php">Logout</a></li>
